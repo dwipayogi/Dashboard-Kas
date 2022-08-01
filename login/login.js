3a30d1306a0b51dcbd83e6c1fa7b346d6235ae91
Dwipa Yogi <dwipayogi50@gmail.com>
2022-08-01 16:01:50 +0700

    add file

---
+++ login/login.js
@@ -0,0 +1,4 @@
+function loginFunction(){
+    var username = document.getElementsByClassName("user")
+    var password = document.getElementsByClassName("pass")
+}
---
+++ uang keluar/script.js
@@ -0,0 +1,23 @@
+function popupOn() {
+    document.getElementById("popup-container").style.display = "block";
+};
+
+function popupOff() {
+    document.getElementById("popup-container").style.display = "none";
+};
+
+function deleteOn(){
+    document.getElementById("confirm-container").style.display = "block";
+};
+
+function deleteOff(){
+    document.getElementById("confirm-container").style.display = "none";
+};
+
+function editOn(){
+    document.getElementById("edit-container").style.display = "block";
+};
+
+function editOff(){
+    document.getElementById("edit-container").style.display = "none";
+};
